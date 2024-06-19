5bf713559429550aaab2c16b066e92ad634144ff
frankyspooky <fbbaidoo004@gmail.com>
2024-06-19 21:59:02 +0000

    added menu toggle button to navbar on smaller screens and started media queries with the stats section

--- src/Componenets/Navbar/Navbar.jsx
+++ src/Componenets/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll'
@@ -6,11 +6,16 @@ import { Link } from 'react-scroll'
 
 const Navbar = () => {
 
+
+const[mobileMenu ,setMobileMenu] = useState(false);
+const toggleMenu =()=>{
+  mobileMenu? setMobileMenu (false): setMobileMenu(true);
+}
   return (
     <div>
         <nav className='container dark-nav'>
             <a href="" className='logo'> Design<span>Agency</span></a>
-            <ul>
+            <ul className={mobileMenu?'':'hide-mobile-menu' }>
                 <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li> <Link to='about' smooth={true} offset={-260} duration={500}>About us</Link></li>
                 <li> <Link to='portfolio' smooth={true} offset={-260} duration={500}>Portfolio</Link></li>
@@ -18,6 +23,7 @@ const Navbar = () => {
                 <li><Link to='hero' smooth={true} offset={-260} duration={500}>Login</Link></li>
                 <li> <Link to='hero' smooth={true} offset={-260} duration={500} className='btn'>Register</Link></li>
             </ul>
+            <img src={menu_icon} alt=" a menu icon" className='menu-icon' onClick={toggleMenu} />
 
         </nav>
       
